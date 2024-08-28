b74f45e16f49385d1d8b9e6d492c29ee9f963459
Aniket Prashar <aniketprashar@hotmail.com>
2024-08-29 01:54:20 +0530

    updated

--- virtual-network.tf
+++ virtual-network.tf
@@ -13,7 +13,7 @@ resource "azurerm_virtual_network" "this" {
     for_each = var.virtual_network.subnets
     content {
       name             = each.value.name
-      address_prefixes = each.value.address_prefix
+      address_prefixes = [each.value.address_prefix]
     }
   }
 }
